61fa987f9b8b398defa0f82f38a7da71970582f7
Kelvin Brito <britok30@gmail.com>
2022-05-01 17:58:00 -0400

    add link to work on artist page later

--- client/src/components/ArtistsGrid.tsx
+++ client/src/components/ArtistsGrid.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { SpotifyApi } from '../types';
 
 export const ArtistsGrid = ({
@@ -10,26 +11,32 @@ export const ArtistsGrid = ({
         {artists && artists.length ? (
             <ul className="list-none m-0 p-0 grid gap-3 grid-cols-1 md:grid-cols-3 lg:grid-cols-6">
                 {artists.map((artist, i) => (
-                    <li
-                        className="cursor-pointer rounded-lg bg-gradient-to-t from-violet-500 to-orange-300 hover:scale-105 transition-all ease-out duration-500 shadow-md"
+                    <Link
                         key={i}
+                        className="p-3 md:p-4"
+                        to={`/artists/${artist.id}`}
                     >
-                        <div className="p-3 md:p-4">
-                            {artist.images[0] && (
-                                <div className="relative pt-[100%] mx-auto mb-6">
-                                    <img
-                                        className="object-cover w-full h-full absolute top-0 rounded-full"
-                                        src={artist.images[0].url}
-                                        alt={artist.name}
-                                    />
-                                </div>
-                            )}
-                            <h3 className="mb-2 text-sm overflow-ellipsis">
-                                {artist.name}
-                            </h3>
-                            <p className="text-sm text-gray-200">Artist</p>
-                        </div>
-                    </li>
+                        <li
+                            className="cursor-pointer rounded-lg bg-gradient-to-t from-violet-500 to-orange-300 hover:scale-105 transition-all ease-out duration-500 shadow-md"
+                            key={i}
+                        >
+                            <div className="p-3 md:p-4">
+                                {artist.images[0] && (
+                                    <div className="relative pt-[100%] mx-auto mb-6">
+                                        <img
+                                            className="object-cover w-full h-full absolute top-0 rounded-full"
+                                            src={artist.images[0].url}
+                                            alt={artist.name}
+                                        />
+                                    </div>
+                                )}
+                                <h3 className="mb-2 text-sm overflow-ellipsis">
+                                    {artist.name}
+                                </h3>
+                                <p className="text-sm text-gray-200">Artist</p>
+                            </div>
+                        </li>
+                    </Link>
                 ))}
             </ul>
         ) : (
