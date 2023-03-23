0950552a71edcb35b421f7ca61b43c27f88b9892
Kelvin Brito <britok30@gmail.com>
2023-03-23 16:22:17 -0400

    heroku

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,75 +1,69 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 import {
-    BrowserRouter as Router,
-    Route,
-    Routes,
-    useLocation,
-} from 'react-router-dom';
-import { accessToken, logout } from './spotify';
-import { LoginButton } from './components/LoginButton';
-import { Profile } from './components/Profile';
-import TopArtists from './components/TopArtists';
-import TopTracks from './components/TopTracks';
-import Playlists from './components/Playlists';
-import Playlist from './components/Playlist';
-import Artist from './components/Artist';
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+import { accessToken, logout } from "./spotify";
+import { LoginButton } from "./components/LoginButton";
+import { Profile } from "./components/Profile";
+import TopArtists from "./components/TopArtists";
+import TopTracks from "./components/TopTracks";
+import Playlists from "./components/Playlists";
+import Playlist from "./components/Playlist";
+import Artist from "./components/Artist";
 
 const App = () => {
-    const [token, setToken] = useState<string | boolean | null>();
+  const [token, setToken] = useState<string | boolean | null>();
 
-    useEffect(() => {
-        setToken(accessToken);
-    }, []);
+  useEffect(() => {
+    setToken(accessToken);
+  }, []);
 
-    const LOGIN_URI =
-        process.env.NODE_ENV !== 'production'
-            ? 'http://localhost:8888/login'
-            : 'https://spotify-connect-v1.herokuapp.com/login';
+  const LOGIN_URI =
+    process.env.NODE_ENV !== "production"
+      ? "http://localhost:8888/login"
+      : "https://spotify-connect-v1.herokuapp.com/login";
 
-    return (
-        <div className="text-white bg-neutral-900 h-full relative">
-            {!token ? (
-                <LoginButton href={LOGIN_URI} />
-            ) : (
-                <>
-                    <button
-                        className="absolute z-10 text-white rounded-full p-3 text-sm bg-black top-1 right-2 mt-4"
-                        onClick={logout}
-                    >
-                        Log Out
-                    </button>
-                    <Router>
-                        <ScrollToTop />
-                        <Routes>
-                            <Route
-                                path="/top-artists"
-                                element={<TopArtists />}
-                            />
-                            <Route path="/artists/:id" element={<Artist />} />
-                            <Route path="/top-tracks" element={<TopTracks />} />
-                            <Route
-                                path="/playlists/:id"
-                                element={<Playlist />}
-                            />
-                            <Route path="/playlists" element={<Playlists />} />
-                            <Route path="/" element={<Profile />} />
-                        </Routes>
-                    </Router>
-                </>
-            )}
-        </div>
-    );
+  return (
+    <div className="text-white bg-neutral-900 h-full relative">
+      {!token ? (
+        <LoginButton href={LOGIN_URI} />
+      ) : (
+        <>
+          <button
+            className="absolute z-10 text-white rounded-full p-3 text-sm bg-black top-1 right-2  mt-4"
+            onClick={logout}
+          >
+            Log Out
+          </button>
+          <Router>
+            <ScrollToTop />
+            <Routes>
+              <Route path="/top-artists" element={<TopArtists />} />
+              <Route path="/artists/:id" element={<Artist />} />
+              <Route path="/top-tracks" element={<TopTracks />} />
+              <Route path="/playlists/:id" element={<Playlist />} />
+              <Route path="/playlists" element={<Playlists />} />
+              <Route path="/" element={<Profile />} />
+            </Routes>
+          </Router>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default App;
 
 const ScrollToTop = () => {
-    const { pathname } = useLocation();
+  const { pathname } = useLocation();
 
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, [pathname]);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
-    return null;
+  return null;
 };
